0eefbc03d1abdfaf29c1a001f9aec5b846ea483a
Christopher Sardegna <github@reagentx.net>
2018-04-08 15:39:33 -0700

    Clarify comments

--- gtrends/main.py
+++ gtrends/main.py
@@ -15,18 +15,23 @@ def gen_data(keywords, normalize=True):
     else:
         for keyword in keywords:
             print(f'Getting {keyword}')
+
+            # Build the dataset with the first keyword
             if keyword == keywords[0]:
                 pytrends.build_payload([keyword], cat=0, timeframe='today 5-y', geo='', gprop='')
                 data = pytrends.interest_over_time()
                 continue
+            
+            # After we have the dataset we append the new data
             pytrends.build_payload([keyword], cat=0, timeframe='today 5-y', geo='', gprop='')
             data[keyword] = pytrends.interest_over_time()[keyword]
+
         # Rearrange columns
         cols = list(data.columns.values)
         cols.append(cols.pop(cols.index('isPartial')))
         return data[cols]
 
 
-k = ['Facebook', 'Instagram', 'Twitter', 'Snapchat', 'Pinterest', 'Google', 'Reddit']
+k = ['Facebook', 'Instagram', 'Twitter', 'Snapchat', 'Google', 'Reddit']
 d = gen_data(k, False)
-print(d)
+print(d.head())
