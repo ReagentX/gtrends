d73cf5f7eef94c27f6e7a040f2319fc409fefb51
Christopher Sardegna <github@reagentx.net>
2018-04-08 16:13:55 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='hltv-scraper',
+    name='gtrends',
     version='1.0',
     description='A Python 3 scraper designed to tabulate data from HLTV.org',
     author='Christopher Sardegna',
     author_email='github@reagentx.net',
-    install_requires=['requests', 'requests-cache'],
+    install_requires=['pytrends'],
     packages=find_packages(),
-    scripts=['scripts/scrape-hltv.py', 'scripts/upcoming.py']
+    scripts=['scripts/run.py']
 )
