2dfea31ea9190b1c39f286a29f7a9ad338a70b94
Christopher Sardegna <github@reagentx.net>
2018-04-14 15:49:25 -0700

    Print __repr__

--- scripts/run.py
+++ scripts/run.py
@@ -5,9 +5,12 @@ from gtrends import generate
 keywords = ['Facebook', 'Instagram', 'Twitter', 'Google Plus', 'Reddit']
 gt = generate.GoogleTrendsData(keywords, True)
 
+# Print the status of the request
+print(gt)
+
 # Generate the data
 data = gt.get()
-print(data)
+print(data.head())
 
 # Dump to a CSV file
 # data.to_csv(f'./output/{"".join(keywords)}.csv')
