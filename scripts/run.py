a9ab66890a5f381fcad1534cc69a95bc065a6b0c
Christopher Sardegna <github@reagentx.net>
2018-04-14 16:10:55 -0700

    Add class properties for construction

--- scripts/run.py
+++ scripts/run.py
@@ -1,11 +1,20 @@
 import pandas as pd
 from gtrends import generate
 
-# Criteria
+# Object properties
 keywords = ['Facebook', 'Instagram', 'Twitter', 'Google Plus', 'Reddit']
-gt = generate.GoogleTrendsData(keywords, True)
+normalize = False
+category = 0  # https://github.com/pat310/google-trends-api/wiki/Google-Trends-Categories
+geo = ''  # Empty is world, use 2-letter ISO code
+timezone = 480  # 480 for PST, timezone offset in minutes
+timeframe = 'all'  # See `timeframe` under https://github.com/GeneralMills/pytrends#common-api-parameters
+gprop = ''  # Empty is "Web search", see above link
+
+# Generate the object that will normalize the data
+gt = generate.GoogleTrendsData(keywords, normalize, category, timezone, timeframe, geo, gprop)
 
 # Print the status of the request
+# gt.set_normalize(False)
 print(gt)
 
 # Generate the data
